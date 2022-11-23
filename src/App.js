e66292364ba7e3241162991704ddec088a3a386b
Meredith Jonatan <mjonatan@me.com>
2022-11-23 14:32:59 -0500

    adjust max width, ensure page layout works with header

--- src/App.js
+++ src/App.js
@@ -10,16 +10,6 @@ import AddInventoryItem from "./pages/AddInventoryItem/AddInventoryItem";
 import Header from "./Components/Header/Header";
 import "./App.scss";
 
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import WarehouseList from "./pages/WarehouseList/WarehouseList";
-// import SelectedWarehouse from "./pages/SelectedWarehouse/SelectedWarehouse";
-// import AddWarehouse from "./pages/AddWarehouse/AddWarehouse";
-// import EditWarehouse from "./pages/EditWarehouse/EditWarehouse";
-// import InventoryList from "./pages/InventoryList/InventoryList";
-// import InventoryItem from "./pages/InventoryItem/InventoryItem";
-// import EditInventoryItem from "./pages/EditInventoryItem/EditInventoryItem";
-// import AddInventoryItem from "./pages/AddInventoryItem/AddInventoryItem";
-
 function App() {
   return (
     <>
@@ -27,17 +17,16 @@ function App() {
         <Header />
           <div className="main__background">
             <div className="main__content">
-              <h1>Hello!!!</h1>
-                  <Routes>
-                    <Route path="/" element={<WarehouseList />} />
-                    <Route path="/:id" element={<SelectedWarehouse />} />
-                    <Route path="/add-warehouse" element={<AddWarehouse />} />
-                    <Route path="/edit-warehouse" element={<EditWarehouse />} />
-                    <Route path="/inventory-list" element={<InventoryList />} />
-                    <Route path="/inventory-item" element={<InventoryItem />} />
-                    <Route path="/edit-inventory-item" element={<EditInventoryItem />} />
-                    <Route path="/add-inventory-item" element={<AddInventoryItem />} />
-                  </Routes>
+                <Routes>
+                  <Route path="/" element={<WarehouseList />} />
+                  <Route path="/:id" element={<SelectedWarehouse />} />
+                  <Route path="/add-warehouse" element={<AddWarehouse />} />
+                  <Route path="/edit-warehouse" element={<EditWarehouse />} />
+                  <Route path="/inventory-list" element={<InventoryList />} />
+                  <Route path="/inventory-item" element={<InventoryItem />} />
+                  <Route path="/edit-inventory-item" element={<EditInventoryItem />} />
+                  <Route path="/add-inventory-item" element={<AddInventoryItem />} />
+                </Routes>
             </div>
           </div>
           </BrowserRouter>
