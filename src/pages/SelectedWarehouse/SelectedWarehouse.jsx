bb70694f7de7bf3ddbfbf2d994883f97c5a960c4
Meredith Jonatan <mjonatan@me.com>
2022-11-24 00:25:48 -0500

    create warehouse details tags & style properties

--- src/pages/SelectedWarehouse/SelectedWarehouse.jsx
+++ src/pages/SelectedWarehouse/SelectedWarehouse.jsx
@@ -1,3 +1,12 @@
-function SelectedWarehouse() {}
+import WarehouseDetails from '../../Components/WarehouseDetails/WarehouseDetails';
+
+function SelectedWarehouse() {
+    return (
+        <>
+        <WarehouseDetails/ >
+        {/* Warehouse Inventory goes here */}
+        </>
+    )
+}
 
 export default SelectedWarehouse;
