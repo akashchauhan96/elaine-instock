d64e351e464e71cf87128af64396304e4d7ef9ad
Meredith Jonatan <mjonatan@me.com>
2022-11-27 18:29:07 -0500

    add initial functions & styles, finish styling header

--- src/Components/EditInventoryHeader/EditInventoryHeader.jsx
+++ src/Components/EditInventoryHeader/EditInventoryHeader.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
 import arrowBack from "../../assets/icons/arrow_back-24px.svg";
+import './EditInventoryHeader.scss'
 
 export default function EditInventoryHeader() {
 
     return (
-        <div className="new-inventory__title-wrapper">
-          <Link to="/" className="new-inventory__back-link">
+        <div className="edit-inventory__title-wrapper">
+          <Link to="/" className="edit-inventory__back-link">
             <img
-              className="new-inventory__back-button"
+              className="edit-inventory__back-button"
               src={arrowBack}
               alt="Arrow to direct user to main warehouse page"
             />
           </Link>
-          <h1 className="new-inventory__title">Edit Inventory Item</h1>
+          <h1 className="edit-inventory__title">Edit Inventory Item</h1>
         </div>
     )
 }
