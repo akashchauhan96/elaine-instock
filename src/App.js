c7ace14334e6615daf83916f2fb98b5d6e13258d
Akash Chauhan <akash.sc96@gmail.com>
2022-11-27 17:29:07 -0500

    Complete footer section

--- src/App.js
+++ src/App.js
@@ -8,26 +8,37 @@ import InventoryItem from "./pages/InventoryItem/InventoryItem";
 import EditInventoryItem from "./pages/EditInventoryItem/EditInventoryItem";
 import AddInventoryItem from "./pages/AddInventoryItem/AddInventoryItem";
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 import "./App.scss";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter className="browser-router">
         <Header />
-          <div className="main__background">
-            <div className="main__content">
-                <Routes>
-                  <Route path="/" element={<WarehouseList />} />
-                  <Route path="/warehouse/:warehouseId" element={<SelectedWarehouse />} />
-                  <Route path="/add-warehouse" element={<AddWarehouse />} />
-                  <Route path="/edit-warehouse" element={<EditWarehouse />} />
-                  <Route path="/inventory" element={<InventoryList />} />
-                  <Route path="/inventory/:id" element={<InventoryItem />} />
-                  <Route path="/edit-inventory-item" element={<EditInventoryItem />} />
-                  <Route path="/add-inventory-item" element={<AddInventoryItem />} />
+        <div className="main__background">
+          <div className="main__content">
+            <Routes>
+              <Route path="/" element={<WarehouseList />} />
+              <Route
+                path="/warehouse/:warehouseId"
+                element={<SelectedWarehouse />}
+              />
+              <Route path="/add-warehouse" element={<AddWarehouse />} />
+              <Route path="/edit-warehouse" element={<EditWarehouse />} />
+              <Route path="/inventory" element={<InventoryList />} />
+              <Route path="/inventory/:id" element={<InventoryItem />} />
+              <Route
+                path="/edit-inventory-item"
+                element={<EditInventoryItem />}
+              />
+              <Route
+                path="/add-inventory-item"
+                element={<AddInventoryItem />}
+              />
             </Routes>
           </div>
+          <Footer />
         </div>
       </BrowserRouter>
     </>
