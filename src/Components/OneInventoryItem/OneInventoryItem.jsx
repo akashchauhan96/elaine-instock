6a6973f46a732a104c98a6a40087a97296da9aae
Sepp <seppnasri@gmail.com>
2022-11-28 05:32:26 -0500

    API to get ONE item is working

--- src/Components/OneInventoryItem/OneInventoryItem.jsx
+++ src/Components/OneInventoryItem/OneInventoryItem.jsx
@@ -2,9 +2,32 @@ import "./OneInventoryItem.scss";
 import EditIcon from "../../assets/icons/edit-white-24px.svg";
 import BackArrow from "../../assets/icons/arrow_back-24px.svg";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import React from "react";
 
 function OneInventoryItem() {
   const navigate = useNavigate();
+  const { id } = useParams();
+  const [axiosCall, setAxiosCall] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/inventory/${id}`)
+      .then((response) => {
+        setAxiosCall(response.data);
+        console.log(axiosCall);
+      })
+      .catch((err) => {
+        console.log(
+          `An error occured while trying to access the inventory: ${err}`
+        );
+      });
+  }, [id]);
+  if (!axiosCall) {
+    return <p>loading</p>;
+  }
+
   return (
     <>
       <div className="inventory-item">
@@ -15,7 +38,7 @@ function OneInventoryItem() {
               alt="back arrow"
               onClick={() => navigate("/inventory")}
             />
-            <h1>Television</h1>
+            <h1>{axiosCall.item_name}</h1>
           </div>
           <div
             className="inventory-item__edit-wrapper"
@@ -33,30 +56,27 @@ function OneInventoryItem() {
           <div className="inventory-item__description">
             <div className="inventory-item__details-item">
               <h4 className="inventory-item__subtitle">ITEM DESCRIPTION</h4>
-              <p className="inventory-item__text">
-                This 50", 4K LED TV provides a crystal-clear picture and vivid
-                colors.
-              </p>
+              <p className="inventory-item__text">{axiosCall.description}</p>
             </div>
             <div className="inventory-item__details-item">
               <h4 className="inventory-item__subtitle">CATEGORY:</h4>
-              <p>Electronics</p>
+              <p>{axiosCall.category}</p>
             </div>
           </div>
           <div className="inventory-item__status-wrapper">
             <div className="inventory-item__status-1">
               <div className="inventory-item__details-item">
                 <h4 className="inventory-item__subtitle">Status:</h4>
-                <p>IN STOCK</p>
+                <p>{axiosCall.status}</p>
               </div>
               <div className="inventory-item__details-item">
                 <h4 className="inventory-item__subtitle">Warehouse:</h4>
-                <p>Manhattan</p>
+                <p>{axiosCall.warehouse_id}</p>
               </div>
             </div>
             <div className="inventory-item__status-2">
               <h4 className="inventory-item__subtitle">Quantity:</h4>
-              <p>500</p>
+              <p>{axiosCall.quantity}</p>
             </div>
           </div>
         </section>
